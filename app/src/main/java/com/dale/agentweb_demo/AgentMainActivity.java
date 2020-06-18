239c7298ce9a19f7d8e5c7302b1282e2b6b07655
dale <dale@itcom888.com>
2020-06-18 13:17:03 +0800

    webX

--- app/src/main/java/com/dale/agentweb_demo/AgentMainActivity.java
+++ app/src/main/java/com/dale/agentweb_demo/AgentMainActivity.java
@@ -5,11 +5,62 @@ import androidx.annotation.Nullable;
 import com.dale.xweb.BaseAgentWebActivity;
 
 public class AgentMainActivity extends BaseAgentWebActivity {
-    
+
     @Nullable
     @Override
     protected String getUrl() {
         return "https://chat.5b3x6.com/chat/chatClient/chatbox.jsp?companyID=5889092&configID=3";
     }
+
+
+
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_agent_main);
+//    }
+//
+//    @NonNull
+//    @Override
+//    protected ViewGroup getAgentWebParent() {
+//        return findViewById(R.id.layout);
+//    }
+//
+//
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (mAgentWeb != null && mAgentWeb.handleKeyEvent(keyCode, event)) {
+//            return true;
+//        }
+//
+//        return super.onKeyDown(keyCode, event);
+//    }
+//
+//    @Override
+//    protected int getIndicatorColor() {
+//        return Color.parseColor("#ff0000");
+//    }
+//
+//    @Override
+//    protected void setTitle(WebView view, String title) {
+//        super.setTitle(view, title);
+//        if (!TextUtils.isEmpty(title)) {
+//            if (title.length() > 10) {
+//                title = title.substring(0, 10).concat("...");
+//            }
+//        }
+////        mTitleTextView.setText(title);
+//    }
+//
+//    @Override
+//    protected int getIndicatorHeight() {
+//        return 3;
+//    }
+//
+//    @Nullable
+//    @Override
+//    protected String getUrl() {
+//        return "https://chat.5b3x6.com/chat/chatClient/chatbox.jsp?companyID=5889092&configID=3";
+//    }
 }
 
